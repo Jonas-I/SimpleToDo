ccd2896618dff85b9133dfb10a27743412f0ffbe
Jonas Improgo <jonah082491@yahoo.com>
2018-12-29 22:30:43 -0500

    Created xml layout file, edit_item_box.xml

--- app/src/main/java/com/example/jonasimprogo/preworkqc/MainActivity.java
+++ app/src/main/java/com/example/jonasimprogo/preworkqc/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.jonasimprogo.preworkqc;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         readItems();
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         lvItems = (ListView) findViewById(R.id.lvitems);
@@ -38,6 +42,43 @@ public class MainActivity extends AppCompatActivity {
 
         setupListViewListener();
 
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String) lvItems.getItemAtPosition(position);
+                editText(item, position);
+            }
+        });
+    }
+
+
+    public void editText(String updItem, final int pos) {
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.edit_item_box);
+        final EditText input = (EditText) dialog.findViewById(R.id.etEditItem);
+        input.setText(updItem);
+        Button btnConfirm = (Button) dialog.findViewById(R.id.btnConfirm);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MainActivity", "Editing Item");
+                items.set(pos, input.getText().toString());
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                dialog.dismiss();
+                Toast.makeText(getApplicationContext(), "Item Updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btnCancel = (Button) dialog.findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 
     public void onAddItem(View v) {
@@ -47,8 +88,9 @@ public class MainActivity extends AppCompatActivity {
         etNewItem.setText("");
         writeItems();
         Toast.makeText(getApplicationContext(), "Added to List: " + itemText, Toast.LENGTH_SHORT).show();
-
     }
+
+
 
     private void setupListViewListener() {
         Log.i("MainActivity", "Setting up listener on list view");
